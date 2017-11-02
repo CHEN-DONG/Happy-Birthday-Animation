1efe9cb5061f2599fd8025632fb38ab1c6638205
chendong.xie <chendong.xie@innocellence.com>
2017-11-02 18:18:15 +0800

    update

--- Birthday/src/scripts/main.js
+++ Birthday/src/scripts/main.js
@@ -1,6 +1,8 @@
 require("../styles/main.css");
+let backAudioSrc =  require("../assets/831143.mp3");
+let loadingImg = require("../assets/loadingCat.gif")
 import Typed from 'typed.js';
-import anime from 'animejs'
+import anime from 'animejs';
 
 const width = window.innerWidth;
 const height = window.innerHeight;
@@ -8,13 +10,13 @@ const ctx = document.getElementById('heart-back').getContext('2d');
 const colors = ["#EF5350", "#EC407A", "#AB47BC", "#7E57C2", "#5C6BC0", "#42A5F5", "#29B6F6", "#26C6DA", "#26A69A", "#66BB6A", "#9CCC65", "#D4E157", "#FFEE58", "#FFCA28", "#FFA726", "#FF7043", "#8D6E63", "#BDBDBD", "#78909C"];
 let hearts = [];
 window.onload = function () {
-
-  StartTypeWord();
+  loadResource();
+  startTypeWord();
   initHeartWall();
-  audioAutoPlay('back-music');
+  // audioAutoPlay('back-music');
 }
 
-function StartTypeWord() {
+function startTypeWord() {
 
   let wrep = '<br>^50';
   // let myWords = `Hi! 力文：^1000
@@ -26,14 +28,15 @@ function StartTypeWord() {
   // 生日快乐
   // I Love You`;
 
-  let myWords = `Test Test
+  let myWords = `好久都没见了,好久都没见了
   ${wrep}
   ${wrep}
-  TestTestTestTestTest TestTestTestTestTestTestTest
+  好久都没见了好久都没见了好久都没见了好久都没见了好久都没见了
   ${wrep}
   ${wrep}
-  TestTestTestTestTestTestTest
-  TestTestTestTestTestTestTestTestTestTest`;
+  好久都没见了好久都没见了好久都没见了
+  ${wrep}
+  好久都没见了好久都没见了好久都没见了好久都没见了好久都没见了好久都没见了`;
 
   let options = {
     strings: [myWords],
@@ -89,7 +92,7 @@ function drawHeart(ctx, cntrX, cntrY, vscale, color) {
   heart.cntrY = cntrY;
   heart.alpha = .8;
   heart.color = color;
-  heart.draw = function(){   
+  heart.draw = function(){     
     ctx.beginPath();
     ctx.moveTo(cntrX, cntrY - getY(0) * vscale);
     for (let i = 0; i < 2 * Math.PI; i = i + d) {
@@ -112,21 +115,30 @@ function getY(t) {
   return 13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t);
 }
 
-function audioAutoPlay(id){  
-  var audio = document.getElementById(id),  
-      play = function(){  
-          audio.play();  
-          document.removeEventListener("touchstart",play, false);  
-      };  
-  audio.play();  
-  document.addEventListener("WeixinJSBridgeReady", function () {  
-      play();  
-  }, false);  
-  document.addEventListener('YixinJSBridgeReady', function() {  
-      play();  
-  }, false);  
-  document.addEventListener("touchstart",play, false);  
-}  
+function loadResource() {
+  let loadEl = document.getElementById('loading');
+  let backAudio = new Audio(backAudioSrc);
+  backAudio.onloadedmetadata=function(){
+    loadEl.style.display = 'none';
+  }
+  
+}
+
+// function audioAutoPlay(id){  
+//   var audio = document.getElementById(id),  
+//       play = function(){  
+//           audio.play();  
+//           document.removeEventListener("touchstart",play, false);  
+//       };  
+//   audio.play();  
+//   document.addEventListener("WeixinJSBridgeReady", function () {  
+//       play();  
+//   }, false);  
+//   document.addEventListener('YixinJSBridgeReady', function() {  
+//       play();  
+//   }, false);  
+//   document.addEventListener("touchstart",play, false);  
+// }  
 
 window.addEventListener('resize', initHeartWall);
 
--- Birthday/webpack.config.js
+++ Birthday/webpack.config.js
@@ -1,14 +1,15 @@
 var path = require('path')
+
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: path.join(__dirname, 'src','scripts','main.js'),
   output: {
-    path: path.join(__dirname, 'public'),
-    filename: "bundle.js"
+    path: path.join(__dirname, 'build'),
+    filename: "bundle.js",
   },
   devServer: {
-    contentBase: "./public",//本地服务器所加载的页面所在的目录
+    contentBase: "./build",//本地服务器所加载的页面所在的目录
     historyApiFallback: true,//不跳转
     inline: true//实时刷新
   },
@@ -26,6 +27,22 @@ module.exports = {
         test: /\.html$/,
         use: 'html-loader'
       },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: path.join('assets/[name].[ext]')
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name:  path.join('assets/[name].[ext]')
+        }
+      }
     ]
   },
   plugins: [
@@ -40,5 +57,5 @@ module.exports = {
       hash : true
     })
   ],
-  //devtool: 'eval-source-map'
+  devtool: 'eval-source-map'
 }
